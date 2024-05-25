31cec458b30c717b15672d181aa272f357dc8453
Eyad Ashraf <163561140+eyadwebdeveloper@users.noreply.github.com>
2024-05-25 20:00:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+
+
+alert('Important: Iam Eyad Ashraf the owner of this website, most of the content of this website is FAKE I made this website as a template for my portfolio in future.')
+
+
+
+
+
+// =============== START Header ===============
+const navBtn = document.querySelector('#navbar-toggler');
+const navDiv = document.querySelector('.navbar-collapse');
+navBtn.addEventListener('click', () => {
+    navDiv.classList.toggle('showNav');
+});
+
+// stopping animation and transition during window resizing
+let resizeTimer;
+window.addEventListener('resize', () => {
+    document.body.classList.add('resize-animation-stopper');
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove('resize-animation-stopper');
+    }, 400);
+});
+
+// =============== END Header ===============
+
+// START Scroll to top button
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+    if (pos > 100) {
+      scrollProgress.style.display = "grid";
+    } else {
+      scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+      document.documentElement.scrollTop = 0;
+    });
+    scrollProgress.style.background = `conic-gradient(#03cc65 ${scrollValue}%, #181717 ${scrollValue}%)`;
+  };
+  window.onscroll = calcScrollValue;
+  window.onload = calcScrollValue;
+// END Scroll to top button
+namme = document.getElementById('name').value;
+email = document.getElementById('email').value;
+sub = document.getElementById('sub').value;
+mes = document.getElementById('mes').value;
+document.getElementById('submit').addEventListener('click', () =>{
+  window.location.href =
+                "submitted.html",
+                "_self";
+  namme = '';
+  email = '';
+  sub = '';
+  mes= '';              
+  
+})
+
+
